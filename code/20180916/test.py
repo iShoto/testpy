c5464504ad14dd2e7defcd401df811f8f839a6d6
iShoto <hermestrain@hotmail.co.jp>
2018-09-16 00:54:59 +0900

    Add 20180916 code.

---
+++ code/20180916/test.py
@@ -0,0 +1,10 @@
+# coding: utf-8
+
+from scipy.misc import imread
+import matplotlib.pyplot as plt
+
+img = imread('IMG_1382.JPG')
+plt.imshow(img / 255.)
+plt.axis('off')
+plt.tick_params(labelbottom=False, labelleft=False, labelright=False, labeltop=False)
+plt.savefig('IMG_1382_ex.JPG', bbox_inches='tight', pad_inches=0)
