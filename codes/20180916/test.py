9bb254b78595e7cc1ba099feb6d7186e7b033ae9
iShoto <hermestrain@hotmail.co.jp>
2018-09-19 21:16:06 +0900

    Add figure, but comment.

--- codes/20180916/test.py
+++ codes/20180916/test.py
@@ -4,6 +4,7 @@ from scipy.misc import imread
 import matplotlib.pyplot as plt
 
 img = imread('IMG_1382.JPG')
+#fig = plt.figure(figsize=(10,10), dpi=200)
 plt.imshow(img / 255.)
 plt.axis('off')
 plt.tick_params(labelbottom=False, labelleft=False, labelright=False, labeltop=False)
