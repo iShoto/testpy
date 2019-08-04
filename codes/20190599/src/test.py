8c6128af5b690719668425f0aa8e51563c662d94
iShoto <hermestrain@hotmail.co.jp>
2019-08-05 01:13:36 +0900

    Add.

---
+++ codes/20190599/src/test.py
@@ -0,0 +1,4 @@
+# coding: utf-8
+import os
+
+print(os.path.exists('../data/TheBigShort/TheBigShot.WEBRip.Netflix.en[cc].vtt'))
---
+++ codes/20190805/src/cnn.py
@@ -0,0 +1,11 @@
+# coding: utf-8
+
+import numpy as np
+import json
+from PIL import Image
+import matplotlib.pyplot as plt
+import os
+
+import torch
+import torchvision
+from torchvision import models, transforms
