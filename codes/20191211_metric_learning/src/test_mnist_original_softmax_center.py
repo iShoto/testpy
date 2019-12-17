3bffd1c74f372d49246b3012ae8a538e360e5ac8
iShoto <hermestrain@hotmail.co.jp>
2019-12-17 23:39:44 +0900

    Update to work a testing model appropriately.

--- codes/20191211_metric_learning/src/test_mnist_original_softmax_center.py
+++ codes/20191211_metric_learning/src/test_mnist_original_softmax_center.py
@@ -1,67 +1,21 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
-import torch.optim as optim
-from torchvision import datasets, transforms
-from  torch.utils.data import DataLoader
-import torch.optim.lr_scheduler as lr_scheduler
-from torch.autograd.function import Function
-import torchvision
 
-import matplotlib.pyplot as plt
 import argparse
-from tqdm import trange
-import numpy as np
 from sklearn.metrics import classification_report
 
 from losses import CenterLoss
 from mnistnet import Net
+import mnist_loader
 
 # cf. https://cpp-learning.com/center-loss/
 
 
-def load_dataset(dataset_dir, batch_size):
-	# Dataset
-	transform = transforms.Compose([
-		transforms.ToTensor(),
-		transforms.Normalize((0.1307,), (0.3081,))
-	])
-	testset = datasets.MNIST(dataset_dir, train=False, download=True, transform=transform)
-	test_loader = torch.utils.data.DataLoader(testset, batch_size=batch_size, shuffle=False, num_workers=0)
-	
-	return test_loader
-
-
-def show_data(train_loader):
-	images, labels = iter(train_loader).next()  # train_loader のミニバッチの image を取得
-	img = torchvision.utils.make_grid(images, nrow=12, padding=1)  # nrom*nrom のタイル形状の画像を作る
-	plt.ion()
-	plt.imshow(np.transpose(img.numpy(), (1, 2, 0)))  # 画像を matplotlib 用に変換
-	plt.draw()
-	plt.pause(3)  # Display an image for three seconds.
-
-
-def main():
-	calc_accuracy()
-
-
-def calc_accuracy():
-	args = parse_args()
-
-	# Device
-	device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-	# Model
-	model = Net().to(device)
-	model.load_state_dict(torch.load(args.model_path))
+def test(device, test_loader, model, nllloss, loss_weight, centerloss):
 	model = model.eval()
-	print(model)
-
-	# Dataset
-	test_loader = load_dataset(args.dataset_dir, batch_size=128)
-	show_data(test_loader)
-
+		
 	# Prediciton
+	running_loss = 0.0
 	pred_list = []
 	label_list = []
 	with torch.no_grad():
@@ -70,20 +24,53 @@ def calc_accuracy():
 			imgs, labels = imgs.to(device), labels.to(device)
 			# Predict labels.
 			ip1, pred = model(imgs)
+			# Calculate loss.
+			loss = nllloss(pred, labels) + loss_weight * centerloss(labels, ip1)
 			# Append predictions and labels.
+			running_loss += loss.item()
 			pred_list += [int(p.argmax()) for p in pred]
 			label_list += [int(l) for l in labels]
 
 	# Calculate accuracy.
-	result = classification_report(pred_list, label_list)
-	print(result)
+	result = classification_report(pred_list, label_list, output_dict=True)
+	test_acc = round(result['weighted avg']['f1-score'], 6)
+	test_loss = round(running_loss / len(test_loader.dataset), 6)
+
+	return test_acc, test_loss
+
+
+def main():
+	args = parse_args()
+
+	# Device
+	device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+	# Dataset
+	train_loader, test_loader, classes = mnist_loader.load_dataset(args.dataset_dir, img_show=True)
+
+	# Model
+	model = Net().to(device)
+	model.load_state_dict(torch.load(args.model_path))
+	model = model.eval()
+	print(model)
+
+	# Loss
+	nllloss = nn.NLLLoss().to(device)  # CrossEntropyLoss = log_softmax + NLLLoss
+	loss_weight = 1
+	centerloss = CenterLoss(10, 2).to(device)
+	
+	# Test a model.
+	print('Testing a trained model....')
+	test_acc, test_loss = test(device, test_loader, model, nllloss, loss_weight, centerloss)
+	stdout_temp = 'test acc: {:<8}, test loss: {:<8}'
+	print(stdout_temp.format(test_acc, test_loss))
 
 
 def parse_args():
 	arg_parser = argparse.ArgumentParser(description="parser for focus one")
 
 	arg_parser.add_argument("--dataset_dir", type=str, default='D:/workspace/datasets')
-	arg_parser.add_argument("--model_path", type=str, default='../output/models/mnist_original_softmax_center_epoch_099.pth')
+	arg_parser.add_argument("--model_path", type=str, default='../outputs/models/mnist_original_softmax_center_epoch_099.pth')
 	
 	args = arg_parser.parse_args()
 
