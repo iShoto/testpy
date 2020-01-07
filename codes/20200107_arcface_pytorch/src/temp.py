55d95118d3d694bef0656a4403d7a24e321cf332
iShoto <hermestrain@hotmail.co.jp>
2020-01-07 23:15:52 +0900

    Update to set functions.

--- codes/20200107_arcface_pytorch/src/temp.py
+++ codes/20200107_arcface_pytorch/src/temp.py
@@ -15,14 +15,33 @@ import torchvision
 from config import config
 from utils import visualizer, view_model
 from data import dataset
-from models import resnet
+from models import resnet, metrics
 #from test import *
 import test
 
 
 def main():
+	device = torch.device("cuda")
+
 	model = resnet.resnet18()
 	print(model)
+
+	num_classes = 13938
+	easy_margin = False
+	metric_fc = metrics.ArcMarginProduct(512, num_classes, s=30, m=0.5, easy_margin=easy_margin)
+	
+	model.to(device)
+	model = DataParallel(model)
+	metric_fc.to(device)
+	metric_fc = DataParallel(metric_fc)
+
+	lr = 1e-1  # initial learning rate
+	lr_step = 10
+	weight_decay = 5e-4
+	optimizer = torch.optim.SGD([{'params': model.parameters()}, 
+								 {'params': metric_fc.parameters()}], 
+								 lr=lr, weight_decay=weight_decay)
+	scheduler = StepLR(optimizer, step_size=lr_step, gamma=0.1)
 
 
 
