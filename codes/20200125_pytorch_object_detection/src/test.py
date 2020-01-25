4d69f0a91918e0dc08496d3f88286e83839344e0
iShoto <hermestrain@hotmail.co.jp>
2020-01-25 17:56:43 +0900

    Update object detection directory path.

--- codes/20200125_pytorch_object_detection/src/test.py
+++ codes/20200125_pytorch_object_detection/src/test.py
@@ -28,15 +28,12 @@ def main():
 	model.load_state_dict(torch.load(args.model_weight_path))
 	model = model.to(device)
 
-	calc_score(model, test_data_loader, device)
-	draw_detection_results(model, test_data_loader, device, args)
+	detect_objects(model, test_data_loader, device)
+	#calc_score(model, test_data_loader, device)
+	#draw_detection_results(model, test_data_loader, device, args)
 
 
-def calc_score(model, test_data_loader, device):
-	engine.evaluate(model, test_data_loader, device=device)
-
-
-def draw_detection_results(model, test_data_loader, device, args):
+def detect_objects(model, test_data_loader, device):
 	model.eval()
 
 	# Draw detection results to test images.
@@ -85,6 +82,83 @@ def draw_detection_results(model, test_data_loader, device, args):
 	print('Done.')
 
 
+
+def calc_score(model, test_data_loader, device):
+	engine.evaluate(model, test_data_loader, device=device)
+
+
+def draw_results(model, test_data_loader, device, args):
+	model.eval()
+
+	# Draw detection results to test images.
+	for batch_idx, (inputs, targets) in enumerate(test_data_loader):
+		# Display progress.
+		precentage = int((batch_idx+1)/len(test_data_loader)*100)
+		print('\rDrawing detection results to images... {:>3}%'.format(precentage), end='')
+
+		# Prepare output image data.
+		imgs = [img for img in inputs]
+		img_ids = [target['image_id'].numpy().tolist()[0] for target in targets]
+
+		# Detect objects
+		inputs = [img.to(device) for img in inputs]
+		outputs = model(inputs)
+		for i in range(len(outputs)):
+			# Organize output image.
+			img = imgs[i]
+			img = img.mul(255).permute(1,2,0).byte().numpy()
+			img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+			# Set output image index.
+			img_id = img_ids[i]
+
+			# Organize detection results.
+			scores = [round(d,3) for d in outputs[i]['scores'].tolist()]
+			boxes = [[int(round(b)) for b in box] for box in outputs[i]['boxes'].tolist()]
+			labels = outputs[i]['labels'].tolist()
+			assert len(scores) == len(boxes) == len(labels)
+
+			# Draw detection results to the image.
+			for j in range(len(scores)):
+				if scores[j] < args.score_thresh:
+					continue
+				cv2.rectangle(img, (boxes[j][0], boxes[j][1]), (boxes[j][2], boxes[j][3]), (0,255,0), 2)
+				text = '{}: {}%'.format(labels[j], int(scores[j]*100))
+				cv2.putText(img, text, (boxes[j][0], boxes[j][1]-5), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0,255,0), 2, cv2.LINE_AA)
+
+			# Resize and save the image.
+			if args.visual_img_half == True:
+				h,w,c = img.shape
+				img = cv2.resize(img, (int(w*0.5), int(h*0.5)))
+			cv2.imwrite(args.visual_img_dir+'visual_img_{}.png'.format(str(img_id).zfill(6)), img)
+
+	print('')
+	print('Done.')
+
+
+def draw_results_core(img_path, annos, colormap, drawn_img_path=None, half_img=False):
+	# Draw annotion data on image.
+	img = cv2.imread(img_path)
+	for a in annos:
+		color = colormap[a['label_name']]
+		cv2.rectangle(img, (a['xmin'], a['ymin']), (a['xmax'], a['ymax']), color, 2)
+		text = '{}'.format(a['label_name'])
+		cv2.putText(img, text, (a['xmin'], a['ymin']-5), cv2.FONT_HERSHEY_SIMPLEX, 0.7, color, 2, cv2.LINE_AA)
+
+	# Save or show an image.
+	if drawn_img_path != None:
+		# Resize the image.
+		if args.half_img == True:
+			h,w,c = img.shape
+			img = cv2.resize(img, (int(w*0.5), int(h*0.5)))
+		# Save the image.
+		cv2.imwrite(drawn_img_path, img)
+	else:
+		cv2.imshow('image', img)
+		cv2.waitKey(0)
+		cv2.destroyAllWindows()
+
+
 def parse_args():
 	# Set arguments.
 	arg_parser = argparse.ArgumentParser(description="Image Classification")
@@ -100,7 +174,7 @@ def parse_args():
 	
 	# Results
 	arg_parser.add_argument('--visual_img_dir', default='../experiments/results/visual_images/')
-	arg_parser.add_argument('--visual_img_half', default=1, type=int, help='0 is False, 1 is True.')
+	arg_parser.add_argument('--visual_img_half', default=1, type=int, help='Resize images half. 0 is False, 1 is True.')
 
 	# Others 
 	arg_parser.add_argument('--score_thresh', default=0.75, type=float)
