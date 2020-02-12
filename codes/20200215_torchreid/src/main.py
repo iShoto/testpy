4595fb51ca3000ce53aa2d70a3eff630fbad7c8f
iShoto <hermestrain@hotmail.co.jp>
2020-02-12 23:16:57 +0900

    Update to work torchreid tutorial.

--- codes/20200215_torchreid/src/main.py
+++ codes/20200215_torchreid/src/main.py
@@ -30,7 +30,9 @@ Elapsed 1:45:48
 """
 
 def main():
-	train()
+	#train()
+	#test()
+	vis_rank()
 
 
 def train():
@@ -86,8 +88,120 @@ def train():
 		test_only=False
 	)
 
+def test():
+	# Step 2: construct data manager
+	datamanager = torchreid.data.ImageDataManager(
+		root='D:/workspace/datasets',
+		sources='market1501',
+		targets='market1501',
+		height=256,
+		width=128,
+		batch_size_train=32,
+		batch_size_test=100,
+		transforms=['random_flip', 'random_crop']
+	)
+
+	# Step 3: construct CNN model
+	model = torchreid.models.build_model(
+		name='resnet50',
+		num_classes=datamanager.num_train_pids,
+		loss='softmax',
+		pretrained=True
+	)
+	model = model.cuda()
+	
+	# Step 4: initialise optimiser and learning rate scheduler
+	optimizer = torchreid.optim.build_optimizer(
+		model,
+		optim='adam',
+		lr=0.0003
+	)
+	scheduler = torchreid.optim.build_lr_scheduler(
+		optimizer,
+		lr_scheduler='single_step',
+		stepsize=20
+	)
+
+	weight_path = '../experiments/models/model_market1501_resnet50.pth.tar-60'
+	torchreid.utils.load_pretrained_weights(model, weight_path)
+
+	# Step 5: construct engine
+	engine = torchreid.engine.ImageSoftmaxEngine(
+		datamanager,
+		model,
+		optimizer=optimizer,
+		scheduler=scheduler,
+		label_smooth=True
+	)
+
+	# Step 6: run model training and test
+	engine.run(
+		#save_dir='log/resnet50',
+		save_dir='../experiments/models/checkpoints',
+		max_epoch=60,
+		eval_freq=10,
+		print_freq=10,
+		test_only=True
+	)
+
+
+def vis_rank():
+	# Step 2: construct data manager
+	datamanager = torchreid.data.ImageDataManager(
+		root='D:/workspace/datasets',
+		sources='market1501',
+		targets='market1501',
+		height=256,
+		width=128,
+		batch_size_train=32,
+		batch_size_test=100,
+		transforms=['random_flip', 'random_crop']
+	)
+
+	# Step 3: construct CNN model
+	model = torchreid.models.build_model(
+		name='resnet50',
+		num_classes=datamanager.num_train_pids,
+		loss='softmax',
+		pretrained=True
+	)
+	model = model.cuda()
+	
+	# Step 4: initialise optimiser and learning rate scheduler
+	optimizer = torchreid.optim.build_optimizer(
+		model,
+		optim='adam',
+		lr=0.0003
+	)
+	scheduler = torchreid.optim.build_lr_scheduler(
+		optimizer,
+		lr_scheduler='single_step',
+		stepsize=20
+	)
+
+	weight_path = '../experiments/models/model_market1501_resnet50.pth.tar-60'
+	torchreid.utils.load_pretrained_weights(model, weight_path)
+
+	# Step 5: construct engine
+	engine = torchreid.engine.ImageSoftmaxEngine(
+		datamanager,
+		model,
+		optimizer=optimizer,
+		scheduler=scheduler,
+		label_smooth=True
+	)
+
+	# Step 6: run model training and test
+	engine.run(
+		#save_dir='log/resnet50',
+		save_dir='../experiments/',
+		max_epoch=60,
+		eval_freq=10,
+		print_freq=10,
+		test_only=True,
+		visrank=True
+	)
 
 if __name__ == "__main__":
 	main()
 
-	
