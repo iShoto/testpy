96295140683ed256aad31797f108691a54d84cf3
iShoto <hermestrain@hotmail.co.jp>
2021-10-03 22:31:05 +0900

    Update.

--- codes/20211003_django_ajax/src/mybook/mybook/settings.py
+++ codes/20211003_django_ajax/src/mybook/mybook/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
+import config
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -20,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'e9khhk7#ftfizlc1x_c3!qts^p2wpl8cx_78q^l^%xp#q!s7nn'
+SECRET_KEY = config.secret_key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
